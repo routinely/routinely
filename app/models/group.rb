ddba4fa80120f267e2939a352ef90a2bd212469d
Woongcheol Yang <woongcheol@gmail.com>
2016-01-23 20:43:06 +0900

    fixup! Add Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ActiveRecord::Base
   has_many :users, dependent: :destroy
   has_many :routines, dependent: :destroy
   has_many :sensors, dependent: :destroy
-  has_many :actuators, dependent: :destroy
+  has_many :actors, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
