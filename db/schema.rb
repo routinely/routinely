8c3e8ec9188799ed47acc718361f6c7f1acb55bd
Woongcheol Yang <woongcheol@gmail.com>
2016-01-24 21:47:32 +0900

    fixup! Add User#name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123105121) do
+ActiveRecord::Schema.define(version: 20160124110018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20160123105121) do
     t.string   "remember_token",     limit: 128,                 null: false
     t.boolean  "admin",                          default: false, null: false
     t.integer  "group_id",                                       null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
