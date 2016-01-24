5aea12a6cac09b78d712ab85c76447ebf1565776
Woongcheol Yang <woongcheol@gmail.com>
2016-01-24 20:05:48 +0900

    fixup! Add Routine model

--- app/fields/administrate/field/time.rb
+++ app/fields/administrate/field/time.rb
@@ -4,7 +4,7 @@ module Administrate
   module Field
     class Time < Administrate::Field::Base
       def to_s
-        data.to_formatted_s(:time)
+        data.try(:to_formatted_s, :time)
       end
     end
   end
