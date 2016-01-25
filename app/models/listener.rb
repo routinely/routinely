c9f635881126aad8dd26a03bbef953ec77c19788
Woongcheol Yang <woongcheol@gmail.com>
2016-01-26 00:35:00 +0900

    Add Listener validations on digital sensors

--- app/models/listener.rb
+++ app/models/listener.rb
@@ -6,6 +6,18 @@ class Listener < ActiveRecord::Base
   validates :sensor, presence: true, uniqueness: { scope: :routine }
   validates :gt, absence: true, unless: -> { sensor.digital? }
   validates :lt, absence: true, unless: -> { sensor.digital? }
+  validate -> {
+    unless [gt, lt].any?
+      errors.add(:gt, "digital sensors require at least one numeric conditions")
+      errors.add(:lt, "digital sensors require at least one numeric conditions")
+    end
+  }, if: :digital?
+  validate -> {
+    unless gt < lt
+      errors.add(:gt, "gt should be less than lt")
+      errors.add(:lt, "gt should be less than lt")
+    end
+  }, unless: -> { [gt, lt].none? }
 
   delegate :binary?, :digital?, to: :sensor
 end
