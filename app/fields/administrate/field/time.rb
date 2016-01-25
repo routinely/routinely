d8bad0805b799edd55b6d271cd31a836d6d50d31
Woongcheol Yang <woongcheol@gmail.com>
2016-01-26 03:28:28 +0900

    Move Administrate::Field::Time representation logic to view

--- app/fields/administrate/field/time.rb
+++ app/fields/administrate/field/time.rb
@@ -3,9 +3,6 @@ require "administrate/fields/base"
 module Administrate
   module Field
     class Time < Administrate::Field::Base
-      def to_s
-        data.try(:to_formatted_s, :time)
-      end
     end
   end
 end
