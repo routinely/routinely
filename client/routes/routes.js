21148eb775da48ad6296a6bbd600cb5aa31bf849
Woongcheol Yang <woongcheol@gmail.com>
2016-05-21 03:44:57 +0900

    Put container components in a separate directory

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router';
-import App from 'components/App';
+import RoutinesListPage from 'containers/RoutinesListPage';
 
 module.exports = (
-  <Route path="/routines" component={App}>
+  <Route path="/routines" component={RoutinesListPage}>
   </Route>
 )
