b759ce5fc4793bd292a6eb2ff5e815aa2f16c799
Woongcheol Yang <woongcheol@gmail.com>
2016-05-25 12:12:43 +0900

    Add NinjaBlocks API integration

---
+++ lib/ninja_blocks.rb
@@ -0,0 +1,35 @@
+module NinjaBlocks
+  class Client
+    include HTTParty
+    base_uri "https://api.ninja.is/rest/v0"
+    default_options.update(verify: false) # NinjaBlocks SSL certificate has expired
+
+    def initialize(token)
+      self.class.default_params(user_access_token: token)
+    end
+
+    def rules
+      self.class.get("/rule")
+    end
+
+    def suspend(rule_id)
+      self.class.post("/rule/#{rule_id}/suspend")
+    end
+
+    def unsuspend(rule_id)
+      self.class.delete("/rule/#{rule_id}/suspend")
+    end
+
+    def devices
+      self.class.get("/devices")
+    end
+
+    def data(device_id)
+      self.class.get("/device/#{device_id}/data")
+    end
+  end
+
+  def self.client(token)
+    Client.new(token)
+  end
+end
