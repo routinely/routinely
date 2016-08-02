7281db36ba329da0d6b03066bce6da74ca83a644
Woongcheol Yang <woongcheol@gmail.com>
2016-08-02 16:26:46 +0900

    Add TimeBasedRoutine#crontab

--- app/models/time_based_routine.rb
+++ app/models/time_based_routine.rb
@@ -8,4 +8,17 @@ class TimeBasedRoutine < ActiveRecord::Base
   validates :name, presence: true, uniqueness: { scope: :group }
   validates :triggers_at, presence: true
   validates :group, presence: true
+
+  def crontab
+    days = []
+    days << 0 if repeats_at? :sun
+    days << 1 if repeats_at? :mon
+    days << 2 if repeats_at? :tue
+    days << 3 if repeats_at? :wed
+    days << 4 if repeats_at? :thu
+    days << 5 if repeats_at? :fri
+    days << 6 if repeats_at? :sat
+
+    "#{triggers_at.min} #{triggers_at.hour} * * #{days.join(',')}"
+  end
 end
