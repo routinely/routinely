1af75677667193e426534174455cdb51bdb41812
Woongcheol Yang <woongcheol@gmail.com>
2016-08-20 23:59:14 +0900

    fixup! TimeBasedRoutine has_many callbacks instead of has_one

--- app/models/time_based_routine.rb
+++ app/models/time_based_routine.rb
@@ -4,7 +4,7 @@ class TimeBasedRoutine < ActiveRecord::Base
   belongs_to :group
 
   has_many :callbacks, as: :routine, dependent: :destroy
-  has_many :actors, through: :callback, source: :target, source_type: "Actor"
+  has_many :actors, through: :callbacks, source: :target, source_type: "Actor"
 
   validates :name, presence: true, uniqueness: { scope: :group }
   validates :triggers_at, presence: true
