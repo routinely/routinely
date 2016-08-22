468352dda76a0731cbc0e1fcd478b7b11b327f7c
Woongcheol Yang <woongcheol@gmail.com>
2016-08-23 07:52:07 +0900

    Handle when Flowable#to_flow is empty in Flows::SyncService

--- app/models/concerns/flowable.rb
+++ app/models/concerns/flowable.rb
@@ -2,6 +2,8 @@ module Flowable
   extend ActiveSupport::Concern
 
   def to_flow
+    return {} if callbacks.empty?
+
     serializer = "Nodered::#{self.class.name}Serializer".constantize
     serializer.new(self).as_json
   end
--- app/services/flows/sync_service.rb
+++ app/services/flows/sync_service.rb
@@ -6,7 +6,10 @@ module Flows
 
     def run!
       if @routine.persisted?
-        if @routine.flow_id.present?
+        if @routine.to_flow.empty?
+          client.remove_flow(@routine.flow_id) if @routine.flow_id.present?
+          @routine.update(flow_id: nil)
+        elsif @routine.flow_id.present?
           client.update_flow(@routine.flow_id, @routine.to_flow)
         else
           response = client.add_flow(@routine.to_flow)
