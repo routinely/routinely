06b69178b550ec58d13182f5e22bb9e9e72c57b4
Woongcheol Yang <woongcheol@gmail.com>
2016-08-26 03:13:30 +0900

    Convert payload fields to appropriate JSON types in Actors::Hue

--- app/models/actors/hue.rb
+++ app/models/actors/hue.rb
@@ -7,9 +7,9 @@ module Actors
       hue_id = SecureRandom.uuid
 
       params = {}
-      params[:on] = [payload["on"]] unless payload["on" ].blank? # true, false
-      params[:bri] = [payload["bri"]] unless payload["bri"].blank? # 0-255
-      params[:rgb] = [payload["rgb"]] unless payload["rgb"].blank? # 0-255
+      params[:on] = [payload["on"] == "true"] unless payload["on"].blank? # true, false
+      params[:bri] = [payload["bri"].to_i] unless payload["bri"].blank? # 0-255
+      params[:rgb] = [payload["rgb"].split(",").map(&:to_i)] unless payload["rgb"].blank? # 0-255
 
       return actor_id, [
         {
