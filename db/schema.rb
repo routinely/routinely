fcf4d82a62a5e08acc7432b2f4b91eda27d0e8da
Woongcheol Yang <woongcheol@gmail.com>
2016-08-26 15:27:44 +0900

    Add Users#phone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825181646) do
+ActiveRecord::Schema.define(version: 20160826062530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -184,6 +184,7 @@ ActiveRecord::Schema.define(version: 20160825181646) do
     t.string   "name"
     t.string   "locale",                         default: "ko"
     t.string   "timezone",                       default: "Seoul"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
