32691ef2271c6664bdd5b341daf75ce664e5e2e0
Woongcheol Yang <woongcheol@gmail.com>
2016-09-09 13:16:35 +0900

    Prevent N+1 queries in routines#index

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -2,9 +2,9 @@ class RoutinesController < ApplicationController
   before_action :require_login
 
   def index
-    @periodic_routines = current_user.group.periodic_routines.order(starts_at: :asc)
+    @periodic_routines = current_user.group.periodic_routines.order(starts_at: :asc).includes(:rf_sensor, :sensors, :actors, :dependent_routines)
     @dependent_routines = current_user.group.dependent_routines.includes(:leading_routine, :rf_sensor, :sensors, :actors)
     @time_based_routines = current_user.group.time_based_routines.order(triggers_at: :asc).includes(:actors)
-    @rule_based_routines = current_user.group.rule_based_routines
+    @rule_based_routines = current_user.group.rule_based_routines.includes(:rf_sensor, :sensors, :actors)
   end
 end
